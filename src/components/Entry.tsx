e0d30bb452400b826657170952efcbc6027ada1f
T-Ev <tjerobotics@gmail.com>
2025-03-15 19:00:35 -0400

    updatge

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -1,19 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const Entry: React.FC = () => {
+function Entry() {
   return (
-    <div className="text-center py-5">
-      <h1 className="display-4 mb-4">Welcome</h1>
-      <p className="lead">
-        Welcome to my portfolio. Check out my{" "}
-        <Link to="/home" className="text-decoration-none">
-          projects
-        </Link>
-        !
-      </p>
-    </div>
+    <Router>
+      <div className="text-center py-5">
+        <h1 className="display-4 mb-4">Welcome</h1>
+        <p className="lead">
+          Welcome to my portfolio. Check out my{" "}
+          <Link to="/home" className="text-decoration-none">
+            projects
+          </Link>
+          !
+        </p>
+      </div>
+    </Router>
   );
-};
+}
 
 export default Entry;
